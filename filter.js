f7a27b7bd4990a25af48e4750ea50044e230d965
Jaewoo Kim <kjw3898@gmail.com>
2021-03-25 17:55:41 +0900

    commit v1.3.0 add less button to scroll chat list limit maximam pinned chat to 400 change absolute maxheight to relative reduce function with duplicated code or only one time use code  - due to vertial arrangement video with chatlist

--- filter.js
+++ filter.js
@@ -1,6 +1,7 @@
 var true_check = false;
 var filterPinedUserList = [];
-var lessMode = true;
+var lessMode = false;
+
 const waitForElement = async selector => {
   while (document.querySelector(selector) === null) {
     await new Promise(resolve => requestAnimationFrame(resolve))
@@ -12,17 +13,51 @@ function getFilterList() {
     console.log(response.data);
   });
 }
+
 function setFilterList() {
   chrome.runtime.sendMessage({ method: "setFilterList", filterList: filterPinedUserList }, function (response) {
     console.log(response.data);
   });
 }
 
-let pinedVODChatList;
-let pinedLiveChatList;            // chat messages list
+let temp_nickname = "";
+function PinButtonCilck(e) {
+  if (filterPinedUserList.indexOf(temp_nickname) != -1) {
+    filterPinedUserList.pop(temp_nickname);
+    e.target.innerText = chrome.i18n.getMessage('pinChatting');
+  }
+  else {
+    if (temp_nickname) {
+      filterPinedUserList.push(temp_nickname);
+      e.target.innerText = chrome.i18n.getMessage('unpinChatting');
+    }
+  }
+  setFilterList();
+}
+
+function addPinButton(node) {
+  temp_nickname = node.querySelector('figure[aria-label]').getAttribute('aria-label');
+  let wisperButton = node.querySelector('button[data-a-target="usercard-whisper-button"]');
+  if (wisperButton) {
+    let buttonParent = wisperButton.parentNode.parentNode.parentNode;
+    let pinButtonDiv = wisperButton.parentNode.parentNode.cloneNode(true);
+    let pinButton = pinButtonDiv.querySelector('[data-a-target="usercard-whisper-button"]');
+    pinButton.removeAttribute("data-a-target");
+    pinButton.removeAttribute("data-test-selector");
+    if (filterPinedUserList.indexOf(temp_nickname) != -1) {
+      pinButton.querySelector('[data-a-target="tw-core-button-label-text"]').innerText = chrome.i18n.getMessage('unpinChatting');
+    } else {
+      pinButton.querySelector('[data-a-target="tw-core-button-label-text"]').innerText = chrome.i18n.getMessage('pinChatting');
+    }
+    pinButton.addEventListener("click", PinButtonCilck, false);
+    buttonParent.insertBefore(pinButtonDiv, buttonParent.childNodes[2]);
+  }
+}
+
+let pinedVODChatList;             // VOD chat messeage list
+let pinedLiveChatList;            // live chat messages list
 let scrollableChatList;           // main chat list (use for resizing)
 let pinnedLiveChatScrollView;     // scrollable live chat view
-const maxChatHeight = 200;        // Max height for pinned chats
 
 const VODObserver = new MutationObserver(function (mutations) {
   mutations.forEach(function (mutation) {
@@ -52,60 +87,16 @@ const LIVEObserver = new MutationObserver(function (mutations) {
         let nickname = node.querySelector('[data-a-user]').innerText;
         if (filterPinedUserList.indexOf(username) != -1 || filterPinedUserList.indexOf(nickname) != -1) {
           let fixChat = node.cloneNode(true);
-          //fixChat.style.cssText = 'padding-left: 0px; padding-right: 0px';
-          //fixChat.querySelector('.chat-line__username-container').firstChild.remove();
           pinedLiveChatList.appendChild(fixChat);
-
-          ResizePinnnedChatList(node);
-          //hideChatOverTwo();
+          resizePinnedChatList();
+          if(pinedLiveChatList.childElementCount>400){
+            pinedLiveChatList.removeChild(pinedLiveChatList.firstChild);  //for memory safty remove old chat over 400
+          }
         }
       }
     });
   });
 });
-
-// resize pinned chat list
-function ResizePinnnedChatList(node) {
-  let chatListHeight = scrollableChatList.offsetHeight + node.offsetHeight + (scrollableChatList.offsetHeight > 10 ? 0 : 10);
-  scrollableChatList.style.minHeight = chatListHeight > maxChatHeight ? maxChatHeight + 'px' : chatListHeight + 'px';
-  scrollableChatList.style.maxHeight = scrollableChatList.style.minHeight;
-  // scroll to bottom
-  // TODO: Add a stop if scrolled up
-  pinnedLiveChatScrollView.scrollTop = pinnedLiveChatScrollView.scrollHeight;
-}
-
-let temp_nickname = "";
-function PinButtonCilck(e) {
-  if (filterPinedUserList.indexOf(temp_nickname) != -1) {
-    filterPinedUserList.pop(temp_nickname);
-    e.target.innerText = chrome.i18n.getMessage('pinChatting');
-  }
-  else {
-    if (temp_nickname) {
-      filterPinedUserList.push(temp_nickname);
-      e.target.innerText = chrome.i18n.getMessage('unpinChatting');
-    }
-  }
-  setFilterList();
-}
-function addPinButton(node) {
-  temp_nickname = node.querySelector('figure[aria-label]').getAttribute('aria-label');
-  let wisperButton = node.querySelector('button[data-a-target="usercard-whisper-button"]');
-  if (wisperButton) {
-    let buttonParent = wisperButton.parentNode.parentNode.parentNode;
-    let pinButtonDiv = wisperButton.parentNode.parentNode.cloneNode(true);
-    let pinButton = pinButtonDiv.querySelector('[data-a-target="usercard-whisper-button"]');
-    pinButton.removeAttribute("data-a-target");
-    pinButton.removeAttribute("data-test-selector");
-    if (filterPinedUserList.indexOf(temp_nickname) != -1) {
-      pinButton.querySelector('[data-a-target="tw-core-button-label-text"]').innerText = chrome.i18n.getMessage('unpinChatting');
-    } else {
-      pinButton.querySelector('[data-a-target="tw-core-button-label-text"]').innerText = chrome.i18n.getMessage('pinChatting');
-    }
-    pinButton.addEventListener("click", PinButtonCilck, false);
-    buttonParent.insertBefore(pinButtonDiv, buttonParent.childNodes[2]);
-  }
-}
 const userCardObserver = new MutationObserver(function (mutations) {
   mutations.forEach(function (mutation) {
     mutation.addedNodes.forEach(function (node) {
@@ -123,6 +114,41 @@ const userCardObserver = new MutationObserver(function (mutations) {
     })
   })
 });
+
+function resizePinnedChatList() {
+  let maxHeightPoint = 0.3;  //need relation value for theatre mode (video and chat vertical arrangement)
+  let maxHeight = '30%';
+  let lessThreshold = 76.2;
+  let pinedChatListHeight = pinedLiveChatList.offsetHeight + 12.4; //for safty badge height (each 0.6) + lessbutton(10px)
+  let chatListHeight = scrollableChatList.parentElement.offsetHeight;
+  if (lessMode) {     //in less mode
+    if (pinedChatListHeight > lessThreshold) {
+      maxHeight = lessThreshold + 'px';
+      scrollableChatList.style.minHeight = maxHeight;
+    }
+    else {
+      scrollableChatList.style.minHeight = pinedChatListHeight;
+    }
+  }
+  else {    //not in less mode
+    if ((pinedChatListHeight / chatListHeight) < maxHeightPoint) {
+      scrollableChatList.style.minHeight = pinedChatListHeight + 'px';
+    }
+    else {
+      scrollableChatList.style.minHeight = maxHeight;
+    }
+  }
+  scrollableChatList.style.maxHeight = scrollableChatList.style.minHeight;
+  // scroll to bottom
+  // TODO: Add a stop if scrolled up
+  pinnedLiveChatScrollView.scrollTop = pinnedLiveChatScrollView.scrollHeight;
+}
+
+function lessButtonCilck() { //lessButton Click Event Handler
+  lessMode = !lessMode;
+  resizePinnedChatList();
+}
+
 //Answer background.js handshake
 chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
   if (msg.text === 'check') {
@@ -139,48 +165,12 @@ chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
     sendResponse({ status: 'ok' })
   }
 });
-function hideChatOverTwo() {
-  let parentChatList = document.querySelector('div.chat-input');
-  let chatList = parentChatList.querySelectorAll('div.chat-input .chat-line__message');
-  let moreButtonDiv = parentChatList.firstChild;
-  if (lessMode && chatList.length > 2) {
-    let nowTransform = moreButtonDiv.firstChild.style.transform;
-    if (!nowTransform) {
-      moreButtonDiv.innerHTML = '<svg version="1.1" viewBox="0 0 20 20" x="0px" y="0px" ><g><path d="M13.5 14.5L9 10l4.5-4.5L12 4l-6 6 6 6 1.5-1.5z"></path></g></svg>';
-      moreButtonDiv.setAttribute("style", "display: table;margin-left: auto;margin-right: auto;height: 15px;");
-      moreButtonDiv.firstChild.setAttribute("style", "fill: currentColor;transform: rotate(90deg);height: 15px;");
-      moreButtonDiv.firstChild.addEventListener("click", moreButtonCilck, false);
-    } else if (nowTransform != "rotate(90deg)") {
-      moreButtonDiv.firstChild.setAttribute("style", "fill: currentColor;transform: rotate(90deg);height: 15px;");
-    }
-    for (let i = 0; i < chatList.length - 2; i++) {
-      chatList[i].style.display = "none";
-    }
-  }
-  else {
-    if (chatList.length > 2) {
-      moreButtonDiv.firstChild.setAttribute("style", "fill: currentColor;transform: rotate(270deg);height: 15px;");
-    }
-    for (let i = 0; i < chatList.length; i++) {
-      chatList[i].style.display = "block";
-    }
-  }
-}
-function moreButtonCilck(e) {
-  lessMode = !lessMode;
-  hideChatOverTwo();
-}
-
-// Clear pinned chat list
-function clearPinnedChat() {
-  pinedLiveChatList.innerHTML = '';
-}
 
 function initChatPins() {
   if (!true_check) { return }
   true_check = false;
   getFilterList();
-  waitForElement('[data-a-target="chat-user-card"]').then((selector) => {
+  waitForElement('[data-a-target="chat-user-card"]').then((selector) => { // chat observer
     userCardObserver.disconnect();
     userCardObserver.observe(selector, {
       attributes: false,
@@ -188,6 +178,7 @@ function initChatPins() {
       subtree: true
     });
   });
+  //vod chat observer
   waitForElement('.video-chat__message-list-wrapper').then((selector) => {
     let parentChatList = document.querySelector('div.qa-vod-chat');
     let pinedVODChatListParent = selector.cloneNode(false);
@@ -202,7 +193,9 @@ function initChatPins() {
       subtree: true
     });
   });
+  //live chat observer
   waitForElement('.chat-scrollable-area__message-container').then((selector) => {
+    //prevent duplicate
     if (document.querySelectorAll('.chat-scrollable-area__message-container').length > 1) {
       return;
     }
@@ -212,11 +205,18 @@ function initChatPins() {
     // hide at initization by setting min and max height both needs to be set resizes with main chat
     scrollableChatList.style.minHeight = '0px';
     scrollableChatList.style.maxHeight = '0px';
+    scrollableChatList.style.borderBottom = 'var(--border-width-default) solid var(--color-border-base)';
     pinnedLiveChatScrollView = scrollableChatList.querySelector('.simplebar-scroll-content')
     pinedLiveChatList = scrollableChatList.querySelector('.chat-scrollable-area__message-container');
-    clearPinnedChat();
-    scrollableChatList.classList.add('tw-border-b');
+    pinedLiveChatList.className = pinedLiveChatList.className.replace('tw-pd-b-1', '');
+    pinedLiveChatList.style.padding = '0px';
+    pinedLiveChatList.innerHTML = ''; // Clear pinned chat list
     parentChatList.insertBefore(scrollableChatList, parentChatList.firstChild);
+    var moreButtonDiv = document.createElement("div"); //add less more control button
+    moreButtonDiv.innerHTML = '<div class="tw-flex tw-full-width tw-justify-content-center" style="padding: .25rem"><div class="channel-leaderboard-header-rotating__expand-grabber tw-border-radius-large tw-c-background-alt-2" style="height: 0.5rem; width: 4rem;"></div></div>';
+    moreButtonDiv.addEventListener("click", lessButtonCilck, false);
+    scrollableChatList.appendChild(moreButtonDiv);
+    window.addEventListener('resize', resizePinnedChatList);
     LIVEObserver.disconnect();
     LIVEObserver.observe(selector, {
       attributes: false,
